59d0ea54f9b54a3885159012ea8d6274be8bb189
Tor Norbye <tnorbye@google.com>
2018-06-07 06:02:52 -0700

    109863461: SDK tools distribution is missing jars and has incorrect jar paths.
    Test: makeSdk target
    Change-Id: Idab1b5ba6d28593f527e28619da730794c4da385
    Fixes: 109863461

--- src/main/groovy/com/android/tools/internal/sdk/javalib/SdkJavaLibPlugin.groovy
+++ src/main/groovy/com/android/tools/internal/sdk/javalib/SdkJavaLibPlugin.groovy
@@ -27,8 +27,6 @@ import org.gradle.api.tasks.bundling.Jar
 
 import javax.inject.Inject
 
-import static com.android.tools.internal.BaseTask.isAndroidArtifact
-import static com.android.tools.internal.BaseTask.isAndroidExternalArtifact
 import static com.android.tools.internal.BaseTask.isLocalArtifact
 
 public class SdkJavaLibPlugin extends SdkFilesPlugin {
@@ -106,8 +104,8 @@ public class SdkJavaLibPlugin extends SdkFilesPlugin {
         Set<ResolvedArtifact> artifacts = configuration.resolvedConfiguration.resolvedArtifacts
         for (ResolvedArtifact artifact : artifacts) {
             ModuleVersionIdentifier id = artifact.moduleVersion.id
-            if ((isAndroidArtifact(id) && !isAndroidExternalArtifact(id)) || isLocalArtifact(id)) {
-                // add the shorter name for the android dependencies
+            if (isLocalArtifact(id)) {
+                // add the shorter name for the local dependencies
                 sb.append(' ').append(artifact.moduleVersion.id.name + ".jar")
             } else {
                 // add the full name
