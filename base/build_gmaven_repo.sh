0b4f1f958fc332279a8c51d5861e2634802cc287
Dana Dahlstrom <dahlstrom@google.com>
2018-10-29 19:00:00 -0700

    Add script to build gmaven_repo.zip on AB
    Change-Id: I3c842a7ca1a6403b89418631dc6aa96fc522d540

---
+++ base/build_gmaven_repo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+readonly script_dir="$(dirname $0)"
+readonly dist_dir="$1"
+
+export JAVA_HOME="$(realpath "${script_dir}"/../../../prebuilts/studio/jdk/linux)"
+
+(cd "${script_dir}"/../.. && ./gradlew --init-script buildSrc/base/release.gradle "-PlocalRepo=${dist_dir}/gmaven_repo" :publishLocal) || exit $?
+(cd "${dist_dir}/gmaven_repo" && zip -r --suffixes sha1:md5:xml "${dist_dir}/gmaven_repo.zip" .) || exit $?
