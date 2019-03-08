efd3d98bb50fd98e8226114f8d9cc48c3e1c3ca6
Chris Warrington <cmw@google.com>
2019-03-08 14:30:14 +0000

    Gradle Build: Build and store the offline repo.
    The gmaven zip contains the artifacts we publish to Google's maven
    repository. This includes the Android Gradle Plugin and various other
    libraries, but does not inlcude the prebuilt transitive dependencies.
    
    The offline repo contains the artifacts needed to run the Android Gradle
    Plugin, and so it additionally contains prebuilt transitive
    dependencies, but does not contain extra libraries built from
    studio-master-dev that are not needed by the Android Gradle Plugin.
    
    This change builds the offline repo and points the Gradle Build to use
    the dist directory so that it appears in the artifacts on the Android
    Build server.
    
    Bug: N/A
    Test: Manually verified artifact is present.
    Change-Id: If58362506a44cb8df3b3e78ca171ff77ca77c222

--- base/build_gmaven_repo.sh
+++ base/build_gmaven_repo.sh
@@ -4,6 +4,7 @@ readonly script_dir="$(dirname $0)"
 readonly dist_dir="$1"
 
 export JAVA_HOME="$(realpath "${script_dir}"/../../../prebuilts/studio/jdk/linux)"
+export DIST_DIR=$dist_dir
 
-(cd "${script_dir}"/../.. && ./gradlew --init-script buildSrc/base/release.gradle "-PlocalRepo=${dist_dir}/gmaven_repo" :publishLocal) || exit $?
+(cd "${script_dir}"/../.. && ./gradlew --init-script buildSrc/base/release.gradle "-PlocalRepo=${dist_dir}/gmaven_repo" :publishLocal :zipOfflineRepo) || exit $?
 (cd "${dist_dir}/gmaven_repo" && zip -r --suffixes sha1:md5:xml "${dist_dir}/gmaven_repo.zip" .) || exit $?
