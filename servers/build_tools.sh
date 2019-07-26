eaef61f90d22bd5375e7910a0629d50797a3d4cb
Dana Dahlstrom <dahlstrom@google.com>
2019-07-25 18:30:00 -0700

    When DIST_DIR doesn't exist, don't cp into it
    Change-Id: I54bb375435c22d7accbebdaef0426ffd5fa7427c

--- servers/build_tools.sh
+++ servers/build_tools.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [[ -z "${DIST_DIR}" ]]; then
-  echo "DIST_DIR missing from environment" >&2
-  exit 1
-fi
-
 readonly BAZEL="$(dirname "$0")/../../../tools/base/bazel/bazel"
 
 "${BAZEL}" test //tools/base/sdklib:commandlinetoolstest || exit $?
-BAZEL_BIN="$("${BAZEL}" info bazel-bin)"
-cp -av "${BAZEL_BIN}"/tools/base/sdklib/commandlinetools_*.zip "${DIST_DIR}"
+
+if [[ -d "${DIST_DIR}" ]]; then
+  readonly BAZEL_BIN="$("${BAZEL}" info bazel-bin)"
+  cp -av "${BAZEL_BIN}"/tools/base/sdklib/commandlinetools_*.zip "${DIST_DIR}"
+fi
