2eea5ea391e971919a674352a477ab915d4d1c19
Chris Warrington <cmw@google.com>
2019-08-07 14:38:57 +0100

    Fix output of offline repo zip.
    It was relying on the gradle build respecting the DIST_DIR environment
    variable, which it no longer does after
    https://android.googlesource.com/platform/tools/buildSrc/+/999bb83c80594d6c08cd83b5a98d836711e14edf
    
    This also stops putting the unzipped local repo in dist/ as it is zipped
    into gmaven_repo anyway.
    
    Test: Manually verified content of dist dir when running tools/buildSrc/base/build_gmaven_repo.sh /tmp/dist_dir
    Fixes: 139008549
    Change-Id: I68432f2d49a6b55b61940093e712a4aa75d4d1be

--- base/build_gmaven_repo.sh
+++ base/build_gmaven_repo.sh
@@ -2,9 +2,10 @@
 
 readonly script_dir="$(dirname $0)"
 readonly dist_dir="$1"
+readonly workspace_dir="$(realpath "${script_dir}/../../../")"
 
 export JAVA_HOME="$(realpath "${script_dir}"/../../../prebuilts/studio/jdk/linux)"
-export DIST_DIR=$dist_dir
 
-(cd "${script_dir}"/../.. && ./gradlew --init-script buildSrc/base/release.gradle "-PlocalRepo=${dist_dir}/gmaven_repo" :publishLocal :zipOfflineRepo) || exit $?
-(cd "${dist_dir}/gmaven_repo" && zip -r --suffixes sha1:md5:xml "${dist_dir}/gmaven_repo.zip" .) || exit $?
+(cd "${workspace_dir}"/tools && ./gradlew --init-script buildSrc/base/release.gradle :publishLocal :zipOfflineRepo) || exit $?
+cp "${workspace_dir}/out/dist/offline_repo.zip" "${dist_dir}"
+(cd "${workspace_dir}/out/repo" && zip -r --suffixes sha1:md5:xml "${dist_dir}/gmaven_repo.zip" .) || exit $?
