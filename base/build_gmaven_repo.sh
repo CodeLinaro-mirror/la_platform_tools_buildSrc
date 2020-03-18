f8904cbdf8cc848fa4f7250f927aafac61daf087
Chris Warrington <cmw@google.com>
2020-03-18 13:01:57 +0000

    Build docs alongside gmaven repo.
    Bug: N/A
    Test: Run as part of the gmaven_repo target.
    Change-Id: Ifd27f9e8e30217656563ca4c827a96f4660b8ac9

--- base/build_gmaven_repo.sh
+++ base/build_gmaven_repo.sh
@@ -6,6 +6,7 @@ readonly workspace_dir="$(realpath "${script_dir}/../../../")"
 
 export JAVA_HOME="$(realpath "${script_dir}"/../../../prebuilts/studio/jdk/linux)"
 
-(cd "${workspace_dir}"/tools && ./gradlew --init-script buildSrc/base/release.gradle :publishLocal :zipOfflineRepo) || exit $?
+(cd "${workspace_dir}"/tools && ./gradlew --init-script buildSrc/base/release.gradle :publishLocal :zipOfflineRepo :base:gradle-api:dokkaZip) || exit $?
 cp "${workspace_dir}/out/dist/offline_repo.zip" "${dist_dir}"
-(cd "${workspace_dir}/out/repo" && zip -r --suffixes sha1:md5:xml "${dist_dir}/gmaven_repo.zip" .) || exit $?
+cp "${workspace_dir}/out/dist/documentation.zip" "${dist_dir}"
+(cd "${workspace_dir}/out/repo" && zip -r --suffixes sha1:md5:xml "${dist_dir}/gmaven_repo.zip" .) || exit $?
