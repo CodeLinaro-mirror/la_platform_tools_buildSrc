41bf4d8f6d6fab7be6f37c20ae60ac027af9b662
Xavier Ducrohet <xav@google.com>
2020-04-01 23:30:07 +0000

    Revert "Remove unused buildSrc classes."
    Revert "Remove usage of deprecated plugins."
    
    Revert submission 10920652-clean-up
    
    Reason for revert: Presubmit didn't detect all usages.
    Reverted Changes:
    I5356afcef:Remove usage of deprecated plugins.
    I21b147358:Remove usage of deprecated plugins.
    I3ccd686df:Remove usage of deprecated plugins.
    Iede18e6d5:Remove unused buildSrc classes.
    
    Change-Id: Icf960fca38a08c99202b1e04014cc43f5586f5b3

---
+++ src/main/groovy/com/android/tools/internal/sdk/base/SdkFilesPlugin.groovy
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.internal.sdk.base
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ */
+class SdkFilesPlugin extends BaseSdkPlugin implements Plugin<Project> {
+
+    BaseExtension extension
+
+    @Override
+    void apply(Project project) {
+        super.apply(project)
+
+        extension = project.extensions.create('sdk', BaseExtension, project.getObjects())
+
+        project.afterEvaluate {
+            createCopyTask()
+        }
+    }
+
+    /**
+     * Hooks called in afterEvaluate, before the ToolItem are queried. Allows delaying some work
+     * in case we need some dynamic features (since ToolItem doesn't support closures)
+     */
+    protected void createCopyTaskHook() {
+        // nothing here
+    }
+
+    protected createCopyTask() {
+        createCopyTaskHook()
+
+        for (PlatformConfig platform : extension.getPlatforms()) {
+
+            CopyToolItemsTask copySdkToolsFiles = project.tasks.create(
+                    "copy${platform.name.capitalize()}SdkToolsFiles", CopyToolItemsTask)
+
+            copySdkToolsFiles.items = platform.items
+            copySdkToolsFiles.itemOutputDir = new File(getSdkRoot(), platform.name + File.separatorChar + "tools")
+            copySdkToolsFiles.noticeDir = new File(project.buildDir, "sdk/notices")
+
+            copySdkToolsFiles.dependsOn platform.builtBy
+        }
+    }
+}
