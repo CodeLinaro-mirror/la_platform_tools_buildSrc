fbe862e57d506a46094aa804ba82b595a92b9566
Ivan Gavrilovic <gavra@google.com>
2021-02-11 16:25:11 +0000

    Remove offline repo creation from tools/base Gradle build
    The offline repo is not used any more and this also cleans up
    the build logic, making AGP build compatible with configuration
    caching.
    
    Bug: n/a
    Test: existing
    Change-Id: If347e0b6fc8d4ae907fb2029fa62a658c11c157d

--- base/build_gmaven_repo.sh
+++ base/build_gmaven_repo.sh
@@ -6,7 +6,6 @@ readonly workspace_dir="$(realpath "${script_dir}/../../../")"
 
 export JAVA_HOME="$(realpath "${script_dir}"/../../../prebuilts/studio/jdk/jdk11/linux)"
 
-(cd "${workspace_dir}"/tools && ./gradlew --init-script buildSrc/base/release.gradle :publishLocal :zipOfflineRepo :base:gradle-api:dokkaZip compileTestJava) || exit $?
-cp "${workspace_dir}/out/dist/offline_repo.zip" "${dist_dir}"
+(cd "${workspace_dir}"/tools && ./gradlew --init-script buildSrc/base/release.gradle :publishLocal :base:gradle-api:dokkaZip compileTestJava) || exit $?
 cp "${workspace_dir}/out/dist/documentation.zip" "${dist_dir}"
 (cd "${workspace_dir}/out/repo" && zip -r --suffixes sha1:md5:xml "${dist_dir}/gmaven_repo.zip" .) || exit $?
