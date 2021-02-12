ed524574716f84b9c290507924f76d2a49e053dd
Ivan Gavrilovic <gavra@google.com>
2021-02-12 11:19:02 +0000

    Enable configuration cache for AGP Gradle build
    Update protobuf gradle plugin to 0.8.15, and fix
    publishing pre-compiled plugin.
    
    Also, enable configuration cache in gradle.properties
    and disable it when building gmaven repo (Dokka task
    is still not compatible).
    
    In build scripts, use ProviderFactory to get system
    properties and environment variables.
    
    Bug: n/a
    Test: building with Gradle
    Change-Id: I16b12d3f1b6a277a2f25c5c7e0f8467f4a11a62a

--- base/build_gmaven_repo.sh
+++ base/build_gmaven_repo.sh
@@ -6,6 +6,9 @@ readonly workspace_dir="$(realpath "${script_dir}/../../../")"
 
 export JAVA_HOME="$(realpath "${script_dir}"/../../../prebuilts/studio/jdk/jdk11/linux)"
 
-(cd "${workspace_dir}"/tools && ./gradlew --init-script buildSrc/base/release.gradle :publishLocal :base:gradle-api:dokkaZip compileTestJava) || exit $?
+(cd "${workspace_dir}"/tools && \
+./gradlew --init-script buildSrc/base/release.gradle :publishLocal compileTestJava  && \
+# Dokka is incompatible with configuration cache
+./gradlew --init-script buildSrc/base/release.gradle :base:gradle-api:dokkaZip --no-configuration-cache) || exit $?
 cp "${workspace_dir}/out/dist/documentation.zip" "${dist_dir}"
 (cd "${workspace_dir}/out/repo" && zip -r --suffixes sha1:md5:xml "${dist_dir}/gmaven_repo.zip" .) || exit $?
