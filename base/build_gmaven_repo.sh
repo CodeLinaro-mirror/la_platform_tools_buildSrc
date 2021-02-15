001bfa8d3d758550b1bc63831d3c6ae147b3ea07
Ivan Gavrilovic <gavra@google.com>
2021-02-15 14:17:25 +0000

    Disable configuration caching by default in AGP
    Running unit tests is incompatible with conf caching
    because of https://youtrack.jetbrains.com/issue/IDEA-254884.
    
    This change disables conf caching in AGP, but it still keeps
    it for the post-submit target, to catch any regressions.
    
    Bug: n/a
    Test: existing
    Change-Id: I807f86897cfb275336be3c28fca6b85d9a6df4d6

--- base/build_gmaven_repo.sh
+++ base/build_gmaven_repo.sh
@@ -7,7 +7,7 @@ readonly workspace_dir="$(realpath "${script_dir}/../../../")"
 export JAVA_HOME="$(realpath "${script_dir}"/../../../prebuilts/studio/jdk/jdk11/linux)"
 
 (cd "${workspace_dir}"/tools && \
-./gradlew --init-script buildSrc/base/release.gradle :publishLocal compileTestJava  && \
+./gradlew --init-script buildSrc/base/release.gradle :publishLocal compileTestJava --configuration-cache  && \
 # Dokka is incompatible with configuration cache
 ./gradlew --init-script buildSrc/base/release.gradle :base:gradle-api:dokkaZip --no-configuration-cache) || exit $?
 cp "${workspace_dir}/out/dist/documentation.zip" "${dist_dir}"
