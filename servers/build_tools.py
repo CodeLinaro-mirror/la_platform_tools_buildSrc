c451fe0954519d626e236e880ebca6cc0e203687
Erwin Jansen <jansene@google.com>
2021-09-24 15:09:15 -0700

    Fix native mac build.
    Validation does not yet succeed on native builders.
    
    Change-Id: Iaf5f5ba2c2f337662d3890d15639b204b6ce75eb

--- servers/build_tools.py
+++ servers/build_tools.py
@@ -227,7 +227,7 @@ def main(argv):
         if args.generate:
             bld.generate()
             return
-        elif (crosscompile or target != 'darwin_aarch64'):
+        elif crosscompile or (not target.startswith("darwin")):
             bld.validate()
         else:
             logging.info("Not validating QEMU build.")
