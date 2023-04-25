649b5778bf2676ba09223d75f11cedc780810059
Hakan Mehmed <hmehmed@google.com>
2023-04-25 18:06:23 +0000

    Prevent /api folder from being wiped clean
    Bug: na
    Test: na
    Change-Id: If82b49d959649465efd305d15ded7855bbdc0090

--- src/main/java/com/android/tools/internal/metalava/MetalavaPlugin.java
+++ src/main/java/com/android/tools/internal/metalava/MetalavaPlugin.java
@@ -88,8 +88,10 @@ public class MetalavaPlugin implements Plugin<Project> {
             });
 
             tasks.register("updateMetalavaApi", Sync.class, task -> {
-               task.from(generateApiOutput);
-               task.setDestinationDir(project.file("api"));
+                task.from(generateApiOutput);
+                task.setDestinationDir(project.file("api"));
+                // preserve all files in destination directory
+                task.preserve(patternFilterable -> patternFilterable.include("**/*"));
             });
 
             tasks.withType(JavaExec.class).configureEach(task -> {
