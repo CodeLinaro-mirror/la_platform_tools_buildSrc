65dad2d4f0acd3741169f7c337bf787ff6193629
Will Hollins <whollins@google.com>
2026-04-08 16:12:32 +0100

    Disable ETS on Linux until android.googlesource.com access is resolved
    Bug: 500625966
    Change-Id: I09bb87c38ca88c9c9b22982eebfee7d509bcd9e3

--- servers/build.py
+++ servers/build.py
@@ -173,12 +173,13 @@ def build_aemu(
         ]
     )
 
-    if not env.is_windows():
+    # TODO(whollins): re-enable ETS on Linux.
+    if not env.is_windows() and not env.target_platform.startswith("linux"):
         # ETS zip file does not yet build on Windows.
         release_targets += android_ets_zip
 
     targets = release_targets + test_targets + always_test_targets
-    if not env.is_windows():
+    if not env.is_windows() and not env.target_platform.startswith("linux"):
         # crashpad tests and ETS don't currently run on Windows.
         targets += external_tests + ets_test_targets
 
