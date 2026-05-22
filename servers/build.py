58c0f06c50b5c3d8fd4e25b1412aea9c05faefa5
Kenneth Magic <kmagic@google.com>
2026-05-22 08:22:06 -0700

    Use the build id for presubmit builds.
    Presubmit builds have build ids and this keeps a consistent naming scheme with emu-now.
    
    Bug: 515738642
    Change-Id: I757186d6275763544cd53596646d07a8d45b6b85

--- servers/build.py
+++ servers/build.py
@@ -163,7 +163,6 @@ def build_aemu(
 
     logs_dir = env.dist_dir / "logs"
     (logs_dir / "bazel").mkdir(parents=True, exist_ok=True)
-    build_id = "snapshot" if env.is_presubmit else env.build_id
     bzl_release = bazel.BazelCmd(env, startup_options=startup_options).with_build_flags(
         build_options
         + [
@@ -171,7 +170,7 @@ def build_aemu(
             f"--build_metadata=ab_target={env.build_target}",
             "--verbose_failures",
             "--build_manual_tests",
-            f"--@goldfish//emulator:build_id={build_id}",
+            f"--@goldfish//emulator:build_id={env.build_id}",
         ]
     )
 
